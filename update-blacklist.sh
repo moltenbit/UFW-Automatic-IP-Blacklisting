b95775169eb2ddb117dc2231daa87bdd646d7cf7
sysadt <sysadt@protonmail.com>
2023-11-08 22:22:50 +0100

    Create update-blacklist.sh

---
+++ update-blacklist.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+curl --compressed https://raw.githubusercontent.com/stamparm/ipsum/master/ipsum.txt 2>/dev/null | grep -v "#" | grep -v -E "\s[1-2]$" | cut -f 1 > /opt/ip-blacklist/ip-blacklist.txt
